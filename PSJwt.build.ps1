f03009c89d2fe7c9c869e1ab9923158a80495246
Stefan Stranger <stefan@stranger.nl>
2018-11-25 17:18:19 +0100

    Initial Build script with task to create Help for the Module using PlatyPS.

---
+++ PSJwt.build.ps1
@@ -0,0 +1,22 @@
+<#
+.Synopsis
+	Build script (https://github.com/nightroman/Invoke-Build)
+#>
+
+# use the most strict mode
+Set-StrictMode -Version Latest
+
+try {
+
+    $script:IsWindows = (-not (Get-Variable -Name IsWindows -ErrorAction Ignore)) -or $IsWindows
+    $script:IsLinux = (Get-Variable -Name IsLinux -ErrorAction Ignore) -and $IsLinux
+    $script:IsMacOS = (Get-Variable -Name IsMacOS -ErrorAction Ignore) -and $IsMacOS
+    $script:IsCoreCLR = $PSVersionTable.ContainsKey('PSEdition') -and $PSVersionTable.PSEdition -eq 'Core'
+}
+
+catch { }
+
+task CreateHelp {
+    Import-Module .\PSJwt.psd1 -Force
+    Update-MarkdownHelp .\docs
+}
