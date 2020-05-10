f5fa19a748086753757d13d1877d42a6340e46d7
Stefan Stranger <stefan@stranger.nl>
2020-05-10 15:39:29 +0200

    Added pester tests for newly added header info

--- tests/PSJwt.tests.ps1
+++ tests/PSJwt.tests.ps1
@@ -63,15 +63,27 @@ Describe -Name 'Module PSJwt works' -Fixture {
 Describe -Name 'Test Functions in PSJwt Module' -Fixture {
     Context -Name 'Testing Public Functions' -Fixture {
 
-        It -name 'Passes ConvertFrom-JWT Function' -test {
+        It -name 'Passes ConvertFrom-JWT Function for Payload' -test {
             $Token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJGaXJzdE5hbWUiOiJTdGVmYW4iLCJMYXN0TmFtZSI6IlN0cmFuZ2VyIiwiRGVtbyI6IkVuY29kZSBBY2Nlc3MgVG9rZW4iLCJleHAiOjEzOTMyODY4OTMsImlhdCI6MTM5MzI2ODg5M30.8-YqAPPth3o-C_xO9WFjW5RViAnDe2WrmVyqLRnNEV0'
-            (ConvertFrom-JWT -Token $Token).Demo | Should Be 'Encode Access Token'
+            (ConvertFrom-JWT -Token $Token).PayLoad.Demo | Should Be 'Encode Access Token'
         }
 
-        It -name 'Passes ConvertTo-JWT Function' -test {
+        It -name 'Passes ConvertFrom-JWT Function for extra Header' -test {
+            $Token = 'eyJFbnYiOiJEZW1vIiwidHlwIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJpYXQiOiIxMzkzMjY4ODkzIiwiRGVtbyI6IkVuY29kZSBBY2Nlc3MgVG9rZW4iLCJGaXJzdE5hbWUiOiJTdGVmYW4iLCJleHAiOiIxMzkzMjg2ODkzIiwiTGFzdE5hbWUiOiJTdHJhbmdlciJ9.JFJVUaBIUJmHQUawkK1dH5Iie8tSTTXKFbZZka3_k7Y'
+            (ConvertFrom-JWT -Token $Token).Header.Env | Should Be 'Demo'
+        }
+
+        It -name 'Passes ConvertTo-JWT Function without extra header' -test {
             $Secret = 'qwerty'
             $Result = ( @{'FirstName' = 'Stefan'; 'LastName' = 'Stranger'; 'Demo' = 'Encode Access Token'; 'exp' = '1393286893'; 'iat' = '1393268893'} | ConvertTo-Jwt -secret $secret)  
             $Result.Length | Should Be 229
+        }
+
+        It -name 'Passes ConvertTo-JWT Function with extra header' -test {
+            $Secret = 'qwerty'
+            $Header = @{'Env' = 'Demo'}
+            $Result = ( @{'FirstName' = 'Stefan'; 'LastName' = 'Stranger'; 'Demo' = 'Encode Access Token'; 'exp' = '1393286893'; 'iat' = '1393268893'} | ConvertTo-Jwt -Header $Header -secret $secret)  
+            $Result.Length | Should Be 247
         }
     }
 }
